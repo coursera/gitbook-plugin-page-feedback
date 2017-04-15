8b6b346db8594451626b06ce57ddd6062e1dc812
Arjun Attam <arjunattam@gmail.com>
2017-04-15 15:16:57 +0530

    add styling and handler

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-var fs = require('fs'),
-    footerString = '',
-    hasFooterFile;
+var _ = require('lodash');
+
+var feedbackString;
+
 
 module.exports = {
     hooks: {
         // called on each book & each language book
         'init': function() {
             var cfg = this.config.get('pluginsConfig.page-feedback'), _this = this;
+            elementStyle = 'border: #49c390 2px solid;border-radius: 5px;padding: 5px;margin-left: 10px;color: #49c390;'
+            feedbackString = '<strong>Was this helpful?</strong> <a id="page-feedback-yes" href="#" style="' + elementStyle + '">Yes, thanks!</a> <a id="page-feedback-no" href="#" style="' + elementStyle + '">Not really</a>';
 
-            // try {
-            //     fs.statSync(this.resolve(cfg.filename));
-            // } catch (e) {
-            //     hasFooterFile = false;
-            //     return;
-            // }
+            function sendToSlack(text) {
+                request_data = {
+                    "channel": "docs-feedback",
+                    "text": text,
+                    "username": "Obi-Wan",
+                    "icon_emoji": ":key:"
+                }
 
-            // hasFooterFile = true;
+                $.post("https://hooks.slack.com/services/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", JSON.stringify(request_data), function(data) {
+                    console.log("success", data);
+                    thanksForFeedback();
+                });
+            }
 
-            // this.readFileAsString(cfg.filename)
-            //     .then(function (data) { return _this.renderBlock('markdown',data); }, this.log.error)
-            //     .then(function(html) { footerString = html; }, this.log.error);
 
-            footerString = 'Was this helpful?<br/><a id="page-feedback-yes">Yes, thanks!</a> <a id="page-feedback-no">Not really</a>';
+            function thanksForFeedback() {
+                $("#page-feedback-yes").text("Thank you for the feedback!");
+                $("#page-feedback-no").hide();
+            }
 
+
+
+            $(function() {
+                $("#page-feedback-yes").on("click", function(e) {
+                    console.log('page feedback yes!');
+                    sendToSlack("Someone thinks <" + window.location.href + "|" + document.title + "> is helpful, yay! :raised_hands:");
+                });
+            });
+
+
+            $(function() {
+                $("#page-feedback-no").on("click", function(e) {
+                    console.log('page feedback no!');
+                    sendToSlack("Someone thinks <" + window.location.href + "|" + document.title + "> is not helpful, oops! :dizzy_face:");
+                });
+            });
         },
         'page:before': function(page) {
             // append to the website renderer only
-            page.content = page.content + '\n{% pagefeedback %}' + footerString + '{% endpagefeedback %}';
+            page.content = page.content + '\n{% pagefeedback %}' + feedbackString + '{% endpagefeedback %}';
             return page;
         }
     },
@@ -34,9 +58,10 @@ module.exports = {
     blocks: {
         'pagefeedback': {
             process: function(block) {
-                // TODO - maybe add some hr here
                 return '<div id="page-feedback">' + block.body + '</div>';
             }
         }
-    }
+    },
+
+    website: getHandlerScripts
 };
