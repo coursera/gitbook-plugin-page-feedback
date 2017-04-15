a0caa075f3abe802bc83ec2b2e50f43289eae999
Arjun Attam <arjunattam@gmail.com>
2017-04-15 15:25:35 +0530

    split browser side js

--- index.js
+++ index.js
@@ -10,43 +10,6 @@ module.exports = {
             var cfg = this.config.get('pluginsConfig.page-feedback'), _this = this;
             elementStyle = 'border: #49c390 2px solid;border-radius: 5px;padding: 5px;margin-left: 10px;color: #49c390;'
             feedbackString = '<strong>Was this helpful?</strong> <a id="page-feedback-yes" href="#" style="' + elementStyle + '">Yes, thanks!</a> <a id="page-feedback-no" href="#" style="' + elementStyle + '">Not really</a>';
-
-            function sendToSlack(text) {
-                request_data = {
-                    "channel": "docs-feedback",
-                    "text": text,
-                    "username": "Obi-Wan",
-                    "icon_emoji": ":key:"
-                }
-
-                $.post("https://hooks.slack.com/services/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", JSON.stringify(request_data), function(data) {
-                    console.log("success", data);
-                    thanksForFeedback();
-                });
-            }
-
-
-            function thanksForFeedback() {
-                $("#page-feedback-yes").text("Thank you for the feedback!");
-                $("#page-feedback-no").hide();
-            }
-
-
-
-            $(function() {
-                $("#page-feedback-yes").on("click", function(e) {
-                    console.log('page feedback yes!');
-                    sendToSlack("Someone thinks <" + window.location.href + "|" + document.title + "> is helpful, yay! :raised_hands:");
-                });
-            });
-
-
-            $(function() {
-                $("#page-feedback-no").on("click", function(e) {
-                    console.log('page feedback no!');
-                    sendToSlack("Someone thinks <" + window.location.href + "|" + document.title + "> is not helpful, oops! :dizzy_face:");
-                });
-            });
         },
         'page:before': function(page) {
             // append to the website renderer only
@@ -61,5 +24,11 @@ module.exports = {
                 return '<div id="page-feedback">' + block.body + '</div>';
             }
         }
+    },
+
+    book: {
+        js: [
+            "handler.js"
+        ]
     }
 };
